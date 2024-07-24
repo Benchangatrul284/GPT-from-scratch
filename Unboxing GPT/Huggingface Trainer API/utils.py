7b13ed15112fc6efabc44c75ea499101bdabb53e
benchang <ptst1110.ee10@nycu.edu.tw>
2024-07-24 20:15:45 +0800

    remove dataset.py train.py

--- Unboxing GPT/Huggingface Trainer API/utils.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plot_loss_curve(losses, val_losses):
-    plt.plot(losses, label='train')
-    plt.plot(val_losses, label='validation')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.savefig('loss_curve.png')
